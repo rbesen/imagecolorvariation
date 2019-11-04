7e9a9251f16290dbc82c17e98aca2b380ca252c7
Rafael Besen <rafael@ae.studio>
2019-11-04 18:20:24 -0300

    Receive params in api and create image object

--- app/main/controller/image_controller.py
+++ app/main/controller/image_controller.py
@@ -4,15 +4,17 @@ from flask_cors import cross_origin
 from flask_restplus import Namespace, fields, marshal
 import json
 from PIL import Image
+import urllib.request
 
 api = Namespace('process-image')
 
 @api.route('/')
 @api.response(200, 'Ok.')
 class ShopList(Resource):
-  @api.doc('Receive image and coordinates')
   def post(self):
+    print(request)
     data = request.json
-    im = Image.open(data.get('image_url'))
-    coefficient = None#call service
-    return coefficient
+    im = Image.open(urllib.request.urlopen(data.get('imageUrl')))
+    print(im.size)
+    #coefficient = None#call service
+    #return coefficient
